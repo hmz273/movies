b1c03c4ae74b89e46b04e8ccb23794ef468737bf
hmz273 <73111873+hmz273@users.noreply.github.com>
2022-09-04 17:19:34 +0100

    done  project

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies();
+    searchMovies("batman");
   }, []);
 
   const searchMovies = async (title) => {
